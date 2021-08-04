0bd8f53145270819f3b68e6f2a8f8bb3128ae160
侯伟轩 <wxhou@yunjinginc.com>
2021-08-04 17:10:24 +0800

    update

--- app/tasks.py
+++ app/tasks.py
@@ -1,12 +1,13 @@
-import time
 import requests
+from io import BytesIO
+from base64 import b64encode
 from celery.result import AsyncResult
 from celery.utils.log import get_task_logger
 from celery.signals import task_success, task_failure
-from .extensions import db, scheduler
+from .extensions import db, cache
 from .models import Apistep, Apitest, Work, Report, Bug, CronTabTask
 from .request import HttpRequest
-from .utils import generate_url
+from .utils import generate_url, get_captcha
 from .worker import celery, flask_app
 
 log = get_task_logger(__name__)
@@ -154,3 +155,13 @@ def saver_crontab(pk, t_id, url):
     obj = CronTabTask(**res)
     db.session.add(obj)
     db.session.commit()
+
+
+@celery.task
+def generate_captcha():
+    code, image = get_captcha(width=120, height=40)
+    cache.set("captcha_%s" % code, code)
+    buffer = BytesIO()
+    image.save(buffer, format="JPEG")
+    img_str = b64encode(buffer.getvalue()).decode()
+    return img_str
